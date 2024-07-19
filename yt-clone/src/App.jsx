152a8d47ff905ac98b4c922969ff2fea3245b9da
saurabh kumar <saurabhslite2@gmail.com>
2024-07-19 16:06:04 +0530

    commit2
    complete Sidebar

--- yt-clone/src/App.jsx
+++ yt-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
     <>
       <Navbar/>
+      <Sidebar/>
     </>
   )
 }
