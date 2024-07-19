0e90af3316bcd004b72b062615c253eadde4fa40
saurabh kumar <saurabhslite2@gmail.com>
2024-07-19 19:16:50 +0530

    commit 4
    complete context API

--- yt-clone/src/App.jsx
+++ yt-clone/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { useAuth } from './context/AuthProvider'
 
 const App = () => {
+  const {loading,data} = useAuth()
+  console.log(loading);
+  console.log(data);
   return (
     <>
       <Navbar/>
